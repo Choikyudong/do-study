8931a2705065c45f384426302a694dad24659a81
Choikyudong <slgmgm@naver.com>
2023-03-08 23:36:00 +0900

    Feat: "101일차 커밋~~"
    Buffer를 잘쓰자~
    
    ps) 몸 관리 좀 해야지

---
+++ src/baekjoon/pack1/Soluction24.java
@@ -0,0 +1,22 @@
+package baekjoon.pack1;
+
+import java.io.*;
+
+/**
+ * Question : ??!
+ * Writer : Kyudong
+ * Date : 2023-03-08
+ * Time : 오후 11:33
+ */
+public class Soluction24 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		bw.write(str + "??!");
+		bw.flush();
+	}
+
+}
