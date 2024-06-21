90f3b5ca3eef21c8b73d9d9ecae0c340a3653834
Choikyudong <slgmgm@naver.com>
2024-06-21 23:51:50 +0900

    Feat: 126일
    칵테일 정렬

--- src/pratcie/sort/Sort1.java
+++ src/pratcie/sort/Sort1.java
@@ -1,31 +1,25 @@
 package pratcie.sort;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Sort1 {
 
+	public static final Random random = new Random();
+
 	public static void main(String[] args) {
 		int[] orginArr = makeRandomArray();
-		int[] arr1 = new int[orginArr.length];
-		System.arraycopy(orginArr, 0, arr1, 0, orginArr.length);
-
-		int[] arr2 = new int[orginArr.length];
-		System.arraycopy(orginArr, 0, arr2, 0, orginArr.length);
-
-		int[] arr3 = new int[orginArr.length];
-		System.arraycopy(orginArr, 0, arr3, 0, orginArr.length);
-
-		new Thread(() -> bubbleSort1(arr1)).start();
-		new Thread(() -> bubbleSort2(arr2)).start();
-		new Thread(() -> bubbleSort3(arr3)).start();
+		new Thread(() -> bubbleSort1(Arrays.copyOf(orginArr, orginArr.length))).start();
+		new Thread(() -> bubbleSort2(Arrays.copyOf(orginArr, orginArr.length))).start();
+		new Thread(() -> bubbleSort3(Arrays.copyOf(orginArr, orginArr.length))).start();
+		new Thread(() -> cocktailSort(Arrays.copyOf(orginArr, orginArr.length))).start();
 	}
 
 	public static int[] makeRandomArray() {
-		Random random = new Random();
-		int length = random.nextInt(450000) + 1;
+		int length = 100;
 		int[] array = new int[length];
 		for (int i = 0; i < length; i++) {
-			array[i] = (random.nextInt(10000) * random.nextInt(100000));
+			array[i] = (random.nextInt(100) * random.nextInt(1000));
 		}
 		return array;
 	}
@@ -49,11 +43,11 @@ public class Sort1 {
 		boolean isChanged;
 		for (int i = 0; i < array.length - 1; i++) {
 			isChanged = false;
-			for (int j = 0; j < array.length - 1; j++) {
-				if (array[j] > array[j + 1]) {
-					int temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
+			for (int j = array.length - 1; j > i; j--) {
+				if (array[j - 1] > array[j]) {
+					int temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
 					isChanged = true;
 				}
 			}
@@ -81,6 +75,46 @@ public class Sort1 {
 			k = last;
 		}
 		System.out.println("bubbleSort3 소요시간 : " + (System.nanoTime() - start));
+	}
+
+	// 버블정렬의 개선 버전, 양방향 버블 정렬 또는 셰이커 정렬이라고도 부름
+	public static void cocktailSort(int[] array) {
+		long start = System.nanoTime();
+		boolean isChanged; // 요소가 교환 여부를 추적하는 용도
+		int startIndex = 0;
+		int endIndex = array.length - 1;
+		do {
+			isChanged = false;
+
+			// 왼쪽부터 정렬 시작
+			for (int i = startIndex; i < endIndex; i++) {
+				if (array[i] > array[i + 1]) {
+					int temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					isChanged = true;
+				}
+			}
+			if (!isChanged) {
+				break;
+			}
+
+			isChanged = false;
+			endIndex--;
+
+			// 오른쪽에서 정렬 시작
+			for (int i = endIndex - 1; i >= startIndex; i--) {
+				if (array[i] > array[i + 1]) {
+					int temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					isChanged = true;
+				}
+			}
+
+			startIndex++;
+		} while (isChanged);
+		System.out.println("cocktailSort 소요시간 : " + (System.nanoTime() - start));
 	}
 
 }
