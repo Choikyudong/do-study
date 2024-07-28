52030da72592fd9213bab49ed1fa7ae07c594eac
Choikyudong <slgmgm@naver.com>
2024-07-28 23:08:00 +0900

    Feat: 141

--- src/pratcie/strsearch/StringSearch.java
+++ src/pratcie/strsearch/StringSearch.java
@@ -4,12 +4,14 @@ public class StringSearch {
 
 	public static void main(String[] args) {
 		bruteForce("ABABCDEFGHAA", "ABC");
+		kmp("ABABCDEFGHAA", "ABC");
 	}
 
 	static void bruteForce(String text, String pattern) {
 		int pt = 0; // 원본 String(text)의 포인터
 		int pp = 0; // pattern의 포인터
 
+		int result = -1;
 		while (pt != text.length() && pp != pattern.length()) {
 			if (text.charAt(pt) == pattern.charAt(pp)) {
 				pt++;
@@ -18,20 +20,55 @@ public class StringSearch {
 				pt = pt - pp + 1; // 1을 더하는 이유는 한칸 전진용
 				pp = 0;
 			}
+			if (pp == pattern.length()) {
+				result = pt - pp;
+				break;
+			}
 		}
 		
+		// 결과
+		if (result == -1) {
+			System.out.println("패턴이 없음");
+		} else {
+			System.out.println(result + "번째 문자부터 일치");
+		}
+	}
+
+	static void kmp(String text, String pattern) {
+		int pt = 1;
+		int pp = 0;
+		int[] skip = new int[pattern.length() + 1]; // 건너뛰기 표
+
+		// 건너뛰기 표 작성
+		skip[pt] = 0;
+		while (pt != pattern.length()) {
+			if (pattern.charAt(pt) == pattern.charAt(pp)) {
+				skip[++pt] = ++pp;
+			} else if (pp == 0) {
+				skip[++pt] = pp;
+			} else {
+				pp = skip[pp];
+			}
+		}
+
+		pt = pp = 0;
+		while (pt != text.length() && pp != pattern.length()) {
+			if (text.charAt(pt) == pattern.charAt(pp)) {
+				pt++;
+				pp++;
+			} else if (pp == 0) {
+				pt++;
+			} else {
+				pp = skip[pp];
+			}
+		}
+
 		// 결과
 		int result = pp == pattern.length() ? pt - pp : -1;
 		if (result == -1) {
 			System.out.println("패턴이 없음");
 		} else {
-			int len = 0;
-			for (int i = 0; i < result; i++) {
-				len += text.substring(i, i + 1).getBytes().length;
-			}
-			len += pattern.length();
-			System.out.println((len + 1) + "번째 문자부터 일치");
+			System.out.println(result + "번째 문자부터 일치");
 		}
 	}
-
 }
