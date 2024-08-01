e4baa019fd20a84eccebfd9ac90b589b2b23e0a7
Choikyudong <slgmgm@naver.com>
2024-08-01 23:00:59 +0900

    Feat: 145
    백준 - 18108

---
+++ src/baekjoon/pack2/Soluction2.java
@@ -0,0 +1,15 @@
+package baekjoon.pack2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soluction2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int y = Integer.parseInt(br.readLine());
+		System.out.println(y - 543);
+	}
+
+}
