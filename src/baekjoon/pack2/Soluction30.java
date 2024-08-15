9f076ceba31f97446958c5c22d527f92394b8da2
Choikyudong <slgmgm@naver.com>
2024-08-15 18:37:19 +0900

    Feat: 169
    백준 - 24264
    왜 long을 안썼지?

--- src/baekjoon/pack2/Soluction30.java
+++ src/baekjoon/pack2/Soluction30.java
@@ -3,15 +3,13 @@ package baekjoon.pack2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Soluction30 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		BigInteger result = BigInteger.valueOf((long) n * n);
-		System.out.println(result);
+		long n = Long.parseLong(br.readLine());
+		System.out.println(n * n);
 		System.out.println(2);
 	}
 
