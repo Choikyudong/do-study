453bd48d5c05661918d5fec99e68a3c11033d83c
Choikyudong <slgmgm@naver.com>
2024-08-15 19:20:26 +0900

    Feat: 171
    백준 - 24266

---
+++ src/baekjoon/pack3/Soluction2.java
@@ -0,0 +1,16 @@
+package baekjoon.pack3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soluction2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		System.out.println(n * n * n);
+		System.out.println(3);
+	}
+
+}
