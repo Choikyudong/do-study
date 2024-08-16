24aa3e80a3164250024daaece3fb28c0ed61cb6e
Choikyudong <slgmgm@naver.com>
2024-08-17 00:13:38 +0900

    Feat: 173
    백준 - 24313

---
+++ src/baekjoon/pack3/Soluction4.java
@@ -0,0 +1,26 @@
+package baekjoon.pack3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Soluction4 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a1 = Integer.parseInt(st.nextToken());
+		int a2 = Integer.parseInt(st.nextToken());
+
+		int c = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		if ((a1 * n) + a2 <= c * n && a1 <= c) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+
+}
