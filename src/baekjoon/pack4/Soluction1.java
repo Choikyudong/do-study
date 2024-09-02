efcb5c1cc9a6674dd7217e907dcaea38ecb54413
Choikyudong <slgmgm@naver.com>
2024-09-02 20:53:57 +0900

    Feat: 189
    백준 - 2485

--- src/baekjoon/pack4/Soluction1.java
+++ src/baekjoon/pack4/Soluction1.java
@@ -14,15 +14,22 @@ public class Soluction1 {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		// 2. 최대 공약수 구하기
-		int gcd = gcd(arr[1] - arr[0], arr[2] - arr[1]);
-		for (int i = 2; i < arr.length; i++) {
-			gcd = gcd(gcd, arr[i + 1] - arr[i]);
+		// 2. 각 가로수의 간격 계산
+		int[] gaps = new int[n - 1];
+		for (int i = 0; i < gaps.length; i++) {
+			gaps[i] = arr[i + 1] - arr[i];
+		}
+
+		// 3. 최대 공약수 구하기
+		int gcd = gaps[0];
+		for (int i = 1; i < gaps.length; i++) {
+			gcd = gcd(gcd, gaps[i]);
 		}
 
 		// 4. 결과 게산 및 출력
-		int result = (arr[n - 1] - arr[0]) / gcd + 1 - n;
-		System.out.println(result);
+		int count = (arr[n - 1] - arr[0]) / gcd + 1;
+		int result = count - n;
+		System.out.print(result);
 	}
 
 	private static int gcd(int x, int y) {
