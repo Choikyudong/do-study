b6acce61ee2b56f02572b45334475d58b6f2f7c8
Choikyudong <slgmgm@naver.com>
2024-10-21 23:03:20 +0900

    Up: 220
    백준 - 9461

--- src/baekjoon/pack5/Soluction16.java
+++ src/baekjoon/pack5/Soluction16.java
@@ -21,7 +21,7 @@ public class Soluction16 {
 		arr[1] = 1;
 		arr[2] = 1;
 		for (int i = 3; i < 101; i++) {
-			arr[i] = arr[i - 1] + arr[i - 2];
+			arr[i] = arr[i - 3] + arr[i - 2];
 		}
 
 		StringBuilder sb = new StringBuilder();
