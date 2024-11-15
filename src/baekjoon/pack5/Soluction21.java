cccb4525f3e079cb090cdc3bfcf7f17f1e523c26
Choikyudong <slgmgm@naver.com>
2024-11-15 17:25:59 +0900

    Feat: 222
    백준 - 11654

---
+++ src/baekjoon/pack5/Soluction21.java
@@ -0,0 +1,11 @@
+package baekjoon.pack5;
+
+import java.io.IOException;
+
+public class Soluction21 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.in.read());
+	}
+
+}
