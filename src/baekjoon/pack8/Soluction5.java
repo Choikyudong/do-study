a5650fc5765c0cdaf2853646e662cd06f1a7257f
Choikyudong <slgmgm@naver.com>
2025-01-11 16:33:56 +0900

    Feat: 255
    백준 - 15666

---
+++ src/baekjoon/pack8/Soluction5.java
@@ -0,0 +1,54 @@
+package baekjoon.pack8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Soluction5 {
+
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] printArr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		printArr = new int[M];
+
+		backTracking(0, 0);
+		System.out.println(sb);
+	}
+
+	static void backTracking(int depth, int comp) {
+		if (depth == M) {
+			for (int i : printArr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		int before = 0;
+		for (int i = 0; i < N; i++) {
+			if (before == arr[i] || comp > arr[i]) {
+				continue;
+			}
+			printArr[depth] = arr[i];
+			backTracking(depth + 1, arr[i]);
+			before = arr[i];
+		}
+	}
+
+}
