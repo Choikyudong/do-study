3d42d57666f56ec212fd03fb687c7870d5aac5cf
Choikyudong <slgmgm@naver.com>
2025-01-11 13:30:45 +0900

    Feat: 255
    백준 - 15657

---
+++ src/baekjoon/pack8/Soluction1.java
@@ -0,0 +1,50 @@
+package baekjoon.pack8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Soluction1 {
+
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] printArr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		printArr = new int[M];
+
+		backTracking(0, 0);
+		System.out.println(sb);
+	}
+
+	static void backTracking(int depth, int index) {
+		if (depth == M) {
+			for (int i : printArr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = index; i < N; i++) {
+			printArr[depth] = arr[i];
+			backTracking(depth + 1, i); // 인덱스 번호를 보내자
+		}
+	}
+
+}
