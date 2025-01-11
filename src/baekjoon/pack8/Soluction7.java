f58751442fa9782475551d4c0da272a2a51913c9
Choikyudong <slgmgm@naver.com>
2025-01-11 16:55:42 +0900

    Feat: 255
    백준 - 2441

---
+++ src/baekjoon/pack8/Soluction7.java
@@ -0,0 +1,25 @@
+package baekjoon.pack8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soluction7 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				sb.append(" ");
+			}
+			for (int j = n - i; j > 0; j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
