0b10bd2ca21730f62009f82365154e1a79770728
Choikyudong <slgmgm@naver.com>
2025-04-20 17:34:26 +0900

    Feat: 286
    백준 - 2558
    머리 식히는 문제

---
+++ src/baekjoon/pack10/Soluction11.java
@@ -0,0 +1,15 @@
+package baekjoon.pack10;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Soluction11 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a + b);
+	}
+
+}
