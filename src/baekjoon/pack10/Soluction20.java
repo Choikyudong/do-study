a48d8656b575b2595586fb6e731d4659ed5f552a
Choikyudong <slgmgm@naver.com>
2025-04-26 15:51:16 +0900

    Feat: 291
    백준 - 11719
    EOF 문제임

---
+++ src/baekjoon/pack10/Soluction20.java
@@ -0,0 +1,19 @@
+package baekjoon.pack10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soluction20 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String input;
+		while ((input = br.readLine()) != null) {
+			sb.append(input).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
