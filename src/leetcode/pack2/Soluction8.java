f19f585d48bddb87f725be8a451e4b990b943c59
Choikyudong <slgmgm@naver.com>
2025-05-11 10:41:03 +0900

    Feat: 303
    leetcode - Second Minimum Node In a Binary Tree (참고)
    이진 트리이고 자식이 무조건 2개가 있다는 전제하에 시작되는 문제
    크게 2개로 행동을 나눈다.
    1. 현재 노드의 값과 일치하는지 확인 후 같다면 각 노드의 왼쪽, 오른쪽 탐색
    2. 각 자식 노드의 값들을 반환한다.
    여기서 -1은 더 이상 탐색할 노드의 값이 없다는 표시용으로도 사용된다.

---
+++ src/leetcode/pack2/Soluction8.java
@@ -0,0 +1,85 @@
+package leetcode.pack2;
+
+/**
+ * <a href="https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/?envType=problem-list-v2&envId=depth-first-search">
+ * 문제링크
+ * </a>
+ */
+public class Soluction8 {
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static void main(String[] args) {
+		// 테스트 1
+		TreeNode leftRoot = new TreeNode(2);
+		TreeNode rightRoot = new TreeNode(
+				5,
+				new TreeNode(5),
+				new TreeNode(7)
+		);
+		TreeNode root = new TreeNode(2, leftRoot, rightRoot);
+		System.out.println(findSecondMinimumValue(root)); // 5
+
+		leftRoot = new TreeNode(2);
+		rightRoot = new TreeNode(2);
+		root = new TreeNode(2, leftRoot, rightRoot);
+		System.out.println(findSecondMinimumValue(root)); // -1
+
+		leftRoot = new TreeNode(2);
+		rightRoot = new TreeNode(
+				5,
+				new TreeNode(5),
+				new TreeNode(5)
+		);
+		root = new TreeNode(2, leftRoot, rightRoot);
+		System.out.println(findSecondMinimumValue(root)); // 5
+	}
+
+
+	public static int findSecondMinimumValue(TreeNode node) {
+		if (node == null) {
+			return -1;
+		}
+
+		if (node.left == null && node.right == null) {
+			return -1;
+		}
+
+		int left = node.left == null ? -1 : node.left.val;
+		if (node.val == node.left.val) {
+			left = findSecondMinimumValue(node.left);
+		}
+
+		int right = node.right == null ? -1 : node.right.val;
+		if (node.val == node.right.val) {
+			right = findSecondMinimumValue(node.right);
+		}
+
+		if (left != -1 && right != -1) {
+			return Math.min(left, right);
+		} else if (left != -1) {
+			return left;
+		} else {
+			return right;
+		}
+	}
+
+}
+
