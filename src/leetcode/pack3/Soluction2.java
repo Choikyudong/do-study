c51db7649a4580b11dc3f406385fa19c5c05c5b4
Choikyudong <slgmgm@naver.com>
2025-05-24 23:59:48 +0900

    Feat: 313
    leetcode - 1025. Divisor Game
    매우 당황;

---
+++ src/leetcode/pack3/Soluction2.java
@@ -0,0 +1,26 @@
+package leetcode.pack3;
+
+/**
+ * <a href="https://leetcode.com/problems/divisor-game/description/?envType=problem-list-v2&envId=dynamic-programming">
+ * 문제링크
+ * </a>
+ */
+public class Soluction2 {
+
+	public static void main(String[] args) {
+		// 테스트1
+		System.out.println(divisorGame(2)); // true
+
+		// 테스트2
+		System.out.println(divisorGame(3)); // false
+	}
+
+	public static boolean divisorGame(int n) {
+		int[] dp = new int[n];
+
+
+		return true;
+	}
+
+}
+
