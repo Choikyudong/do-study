c2feb3a8d05f4b548cd5669070390f3b3db0ea64
Choikyudong <slgmgm@naver.com>
2025-06-16 11:45:26 +0900

    Update: 330
    leetcode - 1043. Partition Array for Maximum Sum (참고)
    주어진 k 만큼만 반복하면서 가장 큰 값을 누적하면 됨.

--- src/leetcode/pack4/Solution3.java
+++ src/leetcode/pack4/Solution3.java
@@ -1,45 +1,45 @@
 package leetcode.pack4;
 
+/**
+ * <a href="https://leetcode.com/problems/painting-a-grid-with-three-different-colors/description/?envType=problem-list-v2&envId=dynamic-programming">
+ * 문제링크
+ * </a>
+ */
 public class Solution3 {
 
 	public static void main(String[] args) {
 		Solution3 solution = new Solution3();
 
 		// 테스트1
-		System.out.println(solution.countSquares(new int[][]{
-				{0, 1, 1, 1},
-				{1, 1, 1, 1},
-				{0, 1, 1, 1},
-		})); // 15
+		System.out.println(solution.maxSumAfterPartitioning(
+				new int[]{1,15,7,9,2,5,10},
+				3
+		)); // 84
 
-		// 테스트1
-		System.out.println(solution.countSquares(new int[][]{
-				{1, 0, 1},
-				{1, 1, 0},
-				{1, 1, 0},
-		})); // 7
+		// 테스트2
+		System.out.println(solution.maxSumAfterPartitioning(
+				new int[]{1,4,1,5,7,3,6,1,9,9,3},
+				4
+		)); // 83
+
+		// 테스트3
+		System.out.println(solution.maxSumAfterPartitioning(
+				new int[]{1},
+				1
+		)); // 1
 	}
 
-	public int countSquares(int[][] matrix) {
-		int m = matrix.length;
-		int n = matrix[0].length;
-
-		int[][] dp = new int[m][n];
-		int sum = 0;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 1) {
-					if (i == 0 || j == 0) {
-						dp[i][j] = matrix[i][j];
-					} else {
-						dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
-					}
-					sum += dp[i][j];
-				}
+	public int maxSumAfterPartitioning(int[] arr, int k) {
+		int n = arr.length;
+		int[] dp = new int[n + 1]; // dp[i] = arr[0..i-1]까지 최댓값
+		for (int i = 1; i <= n; i++) {
+			int maxInPartition = 0;
+			for (int j = 1; j <= k && i - j >= 0; j++) {
+				maxInPartition = Math.max(maxInPartition, arr[i - j]);
+				dp[i] = Math.max(dp[i], dp[i - j] + maxInPartition * j);
 			}
 		}
-
-		return sum;
+		return dp[n];
 	}
 
 }
