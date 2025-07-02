f71a8577133d5b931f0b38024c97fb08169fc49f
Choikyudong <slgmgm@naver.com>
2025-07-02 15:20:48 +0900

    Feat: 338
    2396. Strictly Palindromic Number
    처음에 뭐지.. 이 생각이 들었는데 이 문제는 문제 잘체를 읽고 이해하는게 핵심이라고 생각함.

---
+++ src/leetcode/pack4/Solution22.java
@@ -0,0 +1,24 @@
+package leetcode.pack4;
+
+/**
+ * <a href="https://leetcode.com/problems/strictly-palindromic-number/description/">
+ * 문제링크
+ * </a>
+ */
+public class Solution22 {
+
+	public static void main(String[] args) {
+		Solution22 solution = new Solution22();
+
+		// 테스트1 : false
+		System.out.println(solution.isStrictlyPalindromic(9));
+
+		// 테스트2 : false
+		System.out.println(solution.isStrictlyPalindromic(4));
+	}
+
+	public boolean isStrictlyPalindromic(int n) {
+		return false;
+	}
+
+}
